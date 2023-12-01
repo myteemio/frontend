e8aa822b9474839e743d66898fd43da2e133b247
BE-CH <mikkel.l.bech@gmail.com>
2023-12-01 22:21:21 +0100

    Added frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
+import { Metadata } from 'next';
+import { Box, Button, Container, Grid, Typography } from '@mui/material';
+
+export const metadata: Metadata = {
+  title: 'Teemio',
+  description: 'Den varmeste teemio',
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <Box minWidth="100%" minHeight={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+      <Grid container display={'flex'} alignItems={'center'} width={'80%'}>
+        <Grid item xs={7}>
+          <Typography variant="h3" fontWeight={'bold'}>
+            PLANLÆG DIT NÆSTE TEAMEVENT
+          </Typography>
+          <Typography variant="body1" marginTop={2}>
+            Med Teemio kan du helt gratis planlægge dit næste teamevent! Vi samler det hele ét sted og sørger for at du
+            som virksomhed får minimum 5% rabat på all vores aktiviter - så du kan spare tid og penge!
+          </Typography>
+          <Box marginTop={2}>
+            <Button variant="contained" color="primary" size="large" startIcon="">
+              PLANLÆG NU
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={5}>
+          test2
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
